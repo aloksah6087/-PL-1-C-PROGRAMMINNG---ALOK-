17f115b38a1ae448273a28d5b81b20f583bef11a
aloksah6087 <kumaralok6087@gmail.com>
2026-09-20 22:11:27 +0530

    Create as9.c

---
+++ as9.c
@@ -0,0 +1,18 @@
+/*
+Program(9) -> Write a C program to demonstrate the use of single-line and multi-line comments.
+Solution(1) -> Standard comments layout.
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+    // This is an example of a single-line comment
+
+    /*
+       This is an example of a multi-line comment.
+       It allows you to write notes on more than one line.
+    */
+
+    return 0;
+}
